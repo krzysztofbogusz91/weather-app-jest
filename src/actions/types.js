41160f534547762b6ae7e924278cd5f58bd0a5c2
kfbz <krzysztof.bogusz@gft.com>
2018-05-17 12:43:46 +0200

    fixing

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,7 @@
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 export const FETCH_FORECAST = 'FETCH_FORECAST';
 export const USER_CORDS = 'USER_CORDS';
-export const CLEAR = 'CLEAR'
+export const CLEAR = 'CLEAR';
+export const FETCH_ERR = 'FETCH_ERR'
 
 
